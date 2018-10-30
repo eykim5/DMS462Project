4786394268bd2005231dae3d5ea0d58bb48fe43d
Edward Kim <eykim5@buffalo.edu>
2018-10-30 14:14:15 -0400

    sd

---
+++ RPGBuild/Assets/Scripts/BattleUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleUI : MonoBehaviour {
+
+    public GameObject BattleMenuUI;
+    public GameObject InventoryMenuUI;
+    public GameObject SpecialMenuUI;
+
+    public void BattleFight()
+    {
+        Debug.Log("Attacking enemy...");
+    }
+
+    public void BattleInventory()
+    {
+        Debug.Log("Loading Inventory Menu...");
+        BattleMenuUI.SetActive(false);
+        InventoryMenuUI.SetActive(true);
+    }
+    
+    public void BattleSpecial()
+    {
+        Debug.Log("Loading Special Moves Menu...");
+        BattleMenuUI.SetActive(false);
+        SpecialMenuUI.SetActive(true);
+    }
+
+    public void BattleEscape()
+    {
+        Debug.Log("Escaping...");
+    }
+
+    public void InventoryBack()
+    {
+        Debug.Log("Exiting Inventory Menu...");
+        InventoryMenuUI.SetActive(false);
+        BattleMenuUI.SetActive(true);
+    }
+
+    public void SpecialBack()
+    {
+        Debug.Log("Exiting Special Menu...");
+        SpecialMenuUI.SetActive(false);
+        BattleMenuUI.SetActive(true);
+    }
+}
